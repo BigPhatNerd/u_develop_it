2e3f5bc045b0ac67f81741d3935a7e806586a9d6
Wilson <wilsonhorrell@git.training>
2020-08-03 12:08:53 -0500

    Commit parties table

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS candidates;
+
+CREATE TABLE  candidates (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT ,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
+
+);
+
+CREATE TABLE parties(
+id INTEGER PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+description TEXT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO candidates(first_name, last_name, industry_connected)
+VALUES
+("Ronald", "Firbankafjksl", 1),
+("Virginia", "Woolf", 1),
+("Piers", "Gaveston", 0),
+("Charles", "LeRoi", 1),
+("Katherine", "Mansfield", 1),
+("Dora", "Carrington", 0),
+("Edward", "Bellamy", 0),
+("Montague", "Summers", 1),
+("Octavia", "Butler",1),
+("Unica", "Zurn", 1);
+
+INSERT INTO parties(name, description)
+VALUES 
+("JS JUGGERNAUTS", "THE JS juggernauts , eat, breath ,and sleep javascript. They can  build anything you want."),
+("Heroes of HTML", "Want to see a mock-upt turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds! "),
+("Git Gurus", "Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like they do.");
